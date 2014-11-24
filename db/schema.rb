18c821faeea99f738fdbb430adf0ea722a80cc4c
ErikaLim <Erika.megan.lim@gmail.com>
2014-11-24 15:10:49 -0800

    added location to organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124223828) do
+ActiveRecord::Schema.define(version: 20141124225520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141124223828) do
     t.integer  "number_of_employees"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
   create_table "people", force: true do |t|
