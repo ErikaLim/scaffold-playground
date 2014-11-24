16acc16bd04099d394e50943d4c627e6be889c1d
ErikaLim <Erika.megan.lim@gmail.com>
2014-11-24 14:48:23 -0800

    added favorite_colors to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124185312) do
+ActiveRecord::Schema.define(version: 20141124223828) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "organizations", force: true do |t|
     t.string   "name"
@@ -26,6 +29,8 @@ ActiveRecord::Schema.define(version: 20141124185312) do
     t.date     "date_of_birth"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "birthplace"
+    t.string   "favorite_color"
   end
 
 end
